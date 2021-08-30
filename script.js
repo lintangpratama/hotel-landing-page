f662cf2369d155e6e34844454ea83f95cff515de
lintangpratama <lintangajiyogapratama@gmail.com>
2021-08-30 21:40:10 +0700

    Update tablet layout

---
+++ script.js
@@ -0,0 +1,8 @@
+function hamburgerClick() {
+    let x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className =  "navbar";
+    }
+}
