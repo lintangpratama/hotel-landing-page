7d6cf7ba66d3169f8b5e3a302f6006ee320d59f1
lintangpratama <lintangajiyogapratama@gmail.com>
2021-09-01 12:02:25 +0700

    Add responsive smartphone

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function hamburgerClick() {
-    let x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className =  "navbar";
+    let navbar = document.getElementById("navbar");
+    let leftNavbar = document.getElementById("left-navbar");
+    let navbarOptions = document.getElementById("navbar-option");
+    
+    if (navbar.className === "navbar") {
+        navbar.className += " responsive";
+        leftNavbar.className += " responsive";
+        navbarOptions.className += " responsive";
+    }  else {
+        navbar.className =  "navbar";
+        leftNavbar.className = "left-navbar";
+        navbarOptions.className = "navbar-option";
     }
+
 }
